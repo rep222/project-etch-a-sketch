a89f26e60a988ae41ae649bc9f69e2b4935005fb
Ivan Repusic <repusicivan2@gmail.com>
2025-04-23 09:48:07 +0200

    feat: Add basic game mechanic using hover

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container = document.querySelector('.main-container');
 
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+function hover(element, enter, leave){
+    element.addEventListener('mouseenter', enter);
+    element.addEventListener('mouseleave', leave);
+}
 function generateDivs(amount) {
 
     container.innerHTML = '';
@@ -9,14 +21,18 @@ function generateDivs(amount) {
 
     for (let i = 0; i < amount * amount; i++) {
 
-      const box = document.createElement('div');
-
-      box.className = 'box';
-      box.style.width = `${boxSize}px`;
-      box.style.height = `${boxSize}px`;
-
-      container.appendChild(box);
+        const box = document.createElement('div');
+        hover(box, e => {
+            e.target.style.backgroundColor = getRandomColor()}, 
+            e => {
+                e.target.style.backgroundColor = '#ffff'
+            }
+        )
+        box.className = 'box';
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+        container.appendChild(box);
     }
 }
 
-generateDivs(10);
+generateDivs(50);
