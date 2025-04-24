aa0ec8bb25e1ab46974518f5e6118a66c5dd0ffd
Ivan Repusic <repusicivan2@gmail.com>
2025-04-24 17:29:56 +0200

    feat: Add two buttons, size and restart
    Added a button that changes the size of the grid, and also a button that
    restarts the grid with the current number of rows. Styled both buttons
    and changed heading styling.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.main-container');
-
+const sizeBtn = document.querySelector('.sizeBtn');
+const restartBtn = document.querySelector('.restartBtn');
+let numberOfRows = 10;
 function getRandomColor(){
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -8,31 +10,46 @@ function getRandomColor(){
     }
     return color;
 }
+
 function hover(element, enter, leave){
     element.addEventListener('mouseenter', enter);
     element.addEventListener('mouseleave', leave);
 }
+
 function generateDivs(amount) {
-
     container.innerHTML = '';
-
     const containerSize = 680;
-    const boxSize = 680/amount;
-
+    const boxSize = containerSize/amount;
     for (let i = 0; i < amount * amount; i++) {
-
         const box = document.createElement('div');
-        hover(box, e => {
-            e.target.style.backgroundColor = getRandomColor()}, 
-            e => {
-                e.target.style.backgroundColor = '#ffff'
-            }
-        )
         box.className = 'box';
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
+        let randomColor = getRandomColor();
+        hover(box, e => {
+            e.target.style.backgroundColor = randomColor}, 
+            e => {
+                e.target.style.backgroundColor = randomColor
+            }
+        )
         container.appendChild(box);
     }
 }
 
-generateDivs(50);
+generateDivs(numberOfRows);
+
+sizeBtn.addEventListener('click', () => {
+    let newNumberOfRows = prompt('How many rows do you want?');
+    if (newNumberOfRows === null || newNumberOfRows === ''){
+        return;
+    }else if (newNumberOfRows > 100){
+        alert('Sorry, maximum number is 100');
+        return;
+    } else {
+        numberOfRows = newNumberOfRows;
+        generateDivs(numberOfRows);
+    }
+})
+restartBtn.addEventListener('click', ()=> {
+    generateDivs(numberOfRows);
+})
