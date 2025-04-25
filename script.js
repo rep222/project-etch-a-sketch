2ca09d0f3aa2726f6f2d4874ea5862792b370a03
Ivan Repusic <repusicivan2@gmail.com>
2025-04-25 21:38:57 +0200

    feat: Add color button and polish code
    Added a button that toggles between black color or random colors while
    drawing. Also polished the code. Removed the mouseleave part because
    it didn't do anything since background color was already set, fixed
    box class stacking

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.main-container');
 const sizeBtn = document.querySelector('.sizeBtn');
 const restartBtn = document.querySelector('.restartBtn');
+const colorBtn = document.querySelector('.colorBtn');
 let numberOfRows = 10;
+let color = false;
 function getRandomColor(){
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -15,7 +17,15 @@ function hover(element, enter, leave){
     element.addEventListener('mouseenter', enter);
     element.addEventListener('mouseleave', leave);
 }
-
+colorBtn.addEventListener('click', () => {
+    color = !color;
+    generateDivs(numberOfRows);
+    if (color === true) {
+        colorBtn.classList.add('color-mode');
+    } else {
+        colorBtn.classList.remove('color-mode');
+    }
+})
 function generateDivs(amount) {
     container.innerHTML = '';
     const containerSize = 680;
@@ -27,16 +37,17 @@ function generateDivs(amount) {
         box.style.height = `${boxSize}px`;
         let randomColor = getRandomColor();
         let opacity = 10;
+        let currentColor;
         hover(box, e => {
-            e.currentTarget.className += ' box-transition';
-            e.target.style.backgroundColor = randomColor;
+            if (color === true){
+               currentColor = randomColor;
+            } else currentColor = 'black';
             if (opacity < 100){
                 opacity += 15;
             }
+            e.currentTarget.classList.add('box-transition');
             e.target.style.opacity = `${opacity}%`;
-            }, 
-            e => {
-                e.target.style.backgroundColor = randomColor
+            e.target.style.backgroundColor = currentColor;
             }
         )
         container.appendChild(box);
@@ -53,6 +64,9 @@ sizeBtn.addEventListener('click', () => {
         return;
     }else if (newNumberOfRows > 100){
         alert('Sorry, maximum number is 100');
+        return;
+    }else if (newNumberOfRows < 1) {
+        alert('Sorry, minimum number is 1');
         return;
     }else {
         numberOfRows = newNumberOfRows;
